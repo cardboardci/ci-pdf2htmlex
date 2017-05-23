2bc53665ceab51045e65ebbe7bfef2c3c807262a
jrbeverly <jrbeverly@live.com>
2017-05-23 13:15:56 -0400

    Revising the docker project to use the standard

--- src/provision/install-lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex 
-cd /tmp/
-
-apk --update --no-cache add libpng python freetype glib libintl libxml2 libxml2-dev libltdl cairo poppler pango curl make
-
-rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/lib/apt/lists/*
